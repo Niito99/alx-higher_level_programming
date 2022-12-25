442d587cb5b15fc2a46326092585a10b17f322b1
Kali <kali@kali>
2022-12-25 18:08:01 -0500

    Data structures are intriguing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,9 +2,14 @@
 
 def search_replace(mylist, search, replace):
     k = 0
+    new_list = []
+
     for i in mylist:
+        new_list.append(i)
+
+    for i in new_list:
         if i == search:
             mylist[k] = replace
 
         k += 1
-    return mylist
+    return new_list
