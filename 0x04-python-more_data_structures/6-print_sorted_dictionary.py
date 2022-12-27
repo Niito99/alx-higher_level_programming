c66a12c578d490a1f2b40903f68316d0f59273bc
Kali <kali@kali>
2022-12-26 22:02:41 -0500

    Reorder dictionary according to keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictioanary):
+    store = []
+    for i in a_dictionary:
+        store.append(i)
+
+    store.sort()
+    num = len(store)
+    for in range(num):
+        print("{}: {}".format(store[i], a_dictionary[store[i]]))
