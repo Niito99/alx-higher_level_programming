a5ed64c4026a6ab1fcb44a7c5767dc05d34baa2e
Niito99 <therealokine1@gmai.com>
2024-01-03 05:53:28 +0000

    rev alpha

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(26):
+    if i % 2 == 0:
+        if i == 0:
+            b = 122
+        b = int(122 - (2 * (i / 2)))
+    else:
+        b = int(90 - (2 * (i / 2)))
+    print((list(map(chr, range(b, b+1))))[0], end='')
