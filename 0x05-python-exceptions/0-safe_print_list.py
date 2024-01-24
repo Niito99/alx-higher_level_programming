c236fb9630ab1cac298c19b6634ff2dcdd9e17f0
Niito99 <therealokine1@gmai.com>
2024-01-24 05:23:33 +0000

    Index Error Handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     a = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
+    for b in range(x):
+        try:
+            print(my_list[b], end="")
             a += 1
-        print("\n")
-
-    except IndexError:
-        print("Index doesn't exist in the array")
-
+        except IndexError:
+            break
+    print()
     return a
