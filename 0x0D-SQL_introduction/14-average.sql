849eb2e42b8d99b2fd16b35e6e09edb9f854cc78
Daniel Okine <therealokine1@gmail.com>
2024-05-26 10:23:55 -0700

    Compulsory is completed

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- This file contains sql that computes the average of all records on a table in a database
+
+-- This is the command
+
+SELECT AVG(score) AS average FROM second_table;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- This file contains sql that lists all records of a table in a database
+
+-- The sql command that does that
+
+SELECT score, name FROM second_table WHERE name != "NULL" ORDER BY -score;
